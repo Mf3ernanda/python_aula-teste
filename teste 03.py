4de4cc391c1083f2b3393629841cead030486d6b
Maria Fernanda <129779089+Mf3ernanda@users.noreply.github.com>
2023-04-17 21:34:06 -0300

    Add files via upload

---
+++ teste 03.py
@@ -0,0 +1,17 @@
+num_fav = {
+    'jerson': 24,
+    'fernanda': 7,
+    'amanda': 10,
+    'dilberto': 23,
+    'alguém do fundão': 14,
+}
+
+for nome in num_fav.keys():
+    if nome =='Alguém do fundão':
+    print(f'{nome} -> {num_fav[nome]}')
+    print(nome)
+    print(num_fav[nome])
+    print('--------------')
+
+
+
